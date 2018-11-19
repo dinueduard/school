89372b591c9ed15a0a17bea5f1ab2e3c02fe4457
dinueduard <45100311+dinueduard@users.noreply.github.com>
2018-11-19 16:28:35 +0200

    Add files via upload

---
+++ Curs2/src/Data.java
@@ -0,0 +1,16 @@
+public class Data {
+    public static void main(String[] args) {
+        int myAge =25 ;
+        float weight = 82;
+        double height = 123.5;
+        long distance = 321321321321l;
+        char gender = 'm' ;
+        char test = 9;
+        boolean isStudent = true;
+        boolean isTired = true;
+        int test2 = (int) weight;
+        System.out.print(test);
+        System.out.print(test2);
+
+    }
+}
